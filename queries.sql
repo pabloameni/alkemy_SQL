69b408edb4f68adfb3b296db9bd99df0f71675e9
Pablo Menichini <52842336+pabloameni@users.noreply.github.com>
2022-01-08 15:17:02 -0300

    Create queries.sql

---
+++ queries.sql
@@ -0,0 +1,6 @@
+1) SELECT P.nombre, P.apellido, P.fecha_nacimiento FROM PROFESOR P ORDER BY fecha_nacimiento
+2) SELECT * from PROFESOR where salario >= 65000
+3) SELECT * FROM PROFESOR WHERE fecha_nacimiento BETWEEN "1980-1-1" AND "1989-12-31"
+4) SELECT * FROM PROFESOR LIMIT 5
+5) SELECT * FROM PROFESOR WHERE apellido like 'P%'
+6) SELECT * FROM PROFESOR WHERE fecha_nacimiento BETWEEN "1980-1-1" AND "1989-12-31" and salario > 80000
